0c444974497ba35096d855e0a3b049b4386096f3
SantoshGeo21 <121722215+SantoshGeo21@users.noreply.github.com>
2023-02-25 01:04:32 +0530

    Update application.py

--- application.py
+++ application.py
@@ -71,7 +71,7 @@ def index():
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
                 reviews.append(mydict)
-            client = pymongo.MongoClient("mongodb+srv://santu12:santu@123@cluster0.bbs81qi.mongodb.net/?retryWrites=true&w=majority")
+            client = pymongo.MongoClient("mongodb+srv://santu12:santu123@cluster0.bbs81qi.mongodb.net/?retryWrites=true&w=majority")
             db = client['review_scrap']
             review_col = db['review_scrap_data']
             review_col.insert_many(reviews)
